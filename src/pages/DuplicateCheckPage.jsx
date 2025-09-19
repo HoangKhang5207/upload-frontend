001244ee5f5d2c097397a65c1b2c77e9c4ca064a
NamSaber <namdotatks48@gmail.com>
2025-09-19 16:05:18 +0700

    Add default DuplicateCheckPage and SuggestMetadataPage, complete OcrProcessingPage

---
+++ src/pages/DuplicateCheckPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DuplicateCheckPage() {
+    return (
+        <div>
+            <h1>Duplicate Check Page</h1>
+        </div>
+    );
+}
+
+export default DuplicateCheckPage;
---
+++ src/pages/SuggestMetadataPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SuggestMetadataPage() {
+    return (
+        <div>
+            <h1>Suggest Metadata Page</h1>
+        </div>
+    );
+}
+
+export default SuggestMetadataPage;
